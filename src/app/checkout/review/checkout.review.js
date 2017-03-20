10f690815e258d976d81f59701622237bec95642
BrightMahi <mahindar.singh@brighthat.io>
2017-03-20 16:47:15 +0530

    Change.

--- src/app/checkout/review/checkout.review.js
+++ src/app/checkout/review/checkout.review.js
@@ -92,17 +92,17 @@ function checkoutReviewConfig($stateProvider) {
 
 function CheckoutReviewController($rootScope, $scope,  $state, toastr, OrderCloud, ocConfirm, LineItemsList, OrderPaymentsDetail, CurrentPromotions, ocConfirm, CategoryList, ProductList) {
 	var vm = this;
-	vm.vendorLineItemsMap = {};
+	vm.vendorLineItemsMap = {};	
 	
 	vm.payments = OrderPaymentsDetail;
 	vm.lineItems = LineItemsList;
-	
-	console.log('LineItems', vm.lineItems);
+    
+    console.log('LineItems', vm.lineItems);
     console.log('CategoryList :: ', CategoryList);
     console.log('Products :: ', ProductList);
     console.log('vm.lineItems ::' , JSON.stringify(vm.lineItems));
     
-    // watcher on vm.lineItems
+ // watcher on vm.lineItems
     $scope.$watch(function () {
         	return vm.lineItems;
     	}, function(newVal, oldVal){
@@ -180,4 +180,63 @@ function CheckoutReviewController($rootScope, $scope,  $state, toastr, OrderClou
                 }
             });
     });
+
+    function createAddress(order) {
+        MyAddressesModal.Create()
+            .then(function(address) {
+                toastr.success('Address Created', 'Success');
+                order.ShippingAddressID = address.ID;
+                vm.saveShipAddress(order);
+            });
+    }
+
+    function changeShippingAddress(order) {
+        AddressSelectModal.Open('shipping')
+            .then(function(address) {
+                if (address == 'create') {
+                    vm.createAddress(order);
+                } else {
+                    order.ShippingAddressID = address.ID;
+                    vm.saveShipAddress(order);
+                }
+            })
+    }
+
+    function saveShipAddress(order) {
+        if (order && order.ShippingAddressID) {
+            OrderCloud.Orders.Patch(order.ID, {ShippingAddressID: order.ShippingAddressID})
+                .then(function(updatedOrder) {
+                    $rootScope.$broadcast('OC:OrderShipAddressUpdated', updatedOrder);
+                    vm.getShippingRates(order);
+                })
+                .catch(function(ex){
+                    $exceptionHandler(ex);
+                });
+        }
+    }
+
+    function initShippingRates(order) {
+        if (CheckoutConfig.ShippingRates && order.ShippingAddressID) vm.getShippingRates(order);
+    }
+
+    function getShippingRates(order) {
+        vm.shippersAreLoading = true;
+        vm.shippersLoading = ShippingRates.GetRates(order)
+            .then(function(shipments) {
+                vm.shippersAreLoading = false;
+                vm.shippingRates = shipments;
+                vm.analyzeShipments(order);
+            });
+    }
+
+    function analyzeShipments(order) {
+        vm.shippingRates = ShippingRates.AnalyzeShipments(order, vm.shippingRates);
+    }
+
+    function shipperSelected(order) {
+        ShippingRates.ManageShipments(order, vm.shippingRates)
+            .then(function() {
+                $rootScope.$broadcast('OC:UpdateOrder', order.ID);
+            });
+    }
 }
