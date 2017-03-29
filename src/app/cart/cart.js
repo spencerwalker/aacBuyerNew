3d0cc837d63d1433138780c556f32cbfdb7a321f
BrightMahi <mahindar.singh@brighthat.io>
2017-03-29 21:19:20 +0530

    Checking.

--- src/app/cart/cart.js
+++ src/app/cart/cart.js
@@ -73,15 +73,7 @@ function CartController($rootScope, $scope,  $state, toastr, OrderCloud, LineIte
     	angular.forEach(vm.lineItems.Items, function(lineItem){
         	var productId = lineItem.ProductID;
         	var vendorName = productId.split("_")[0]; 
-        	/*
-    	    if(lineItem.ID.match("^[a-zA-Z\(\)]+$")) {  
-    	      } else {
-    	    	 var number = Math.floor(1000000 + Math.random() * 9000000);
-    	    	 lineItem.ID = number;
-    	      }  
-    	    	
-        	lineItem.vendorName = vendorName;
-        	*/
+
         	if(typeof vm.vendorLineItemsMap[vendorName] === 'undefined'){
         		vm.vendorLineItemsMap[vendorName] = [];
         	}
