aeb397b4bc3c532ac649b47ad3381149c5fefe2f
BrightMahi <MC2@DESKTOP-0B4TEMQ>
2017-05-15 12:42:20 +0530

    Code changes for auto login.

--- src/app/autoLogin/autoLogin.js
+++ src/app/autoLogin/autoLogin.js
@@ -8,7 +8,7 @@ function AutoLoginConfig($stateProvider) {
     $stateProvider
         .state('autoLogin', {
             url: '/autoLogin/:token/:catid/:timestamp/:encryptstamp',
-            templateUrl: 'autoLogin/templates/autoLogin.tpl.html',
+            //templateUrl: 'autoLogin/templates/autoLogin.tpl.html',
             controller: 'AutoLoginCtrl',
             controllerAs: 'autoLogin'
         })
@@ -31,35 +31,23 @@ function AutoLoginController($state, $stateParams, $exceptionHandler, OrderCloud
     vm.timestamp = $stateParams.timestamp;
     vm.encryptstamp = $stateParams.encryptstamp;
     vm.catid = $stateParams.catid;
-    console.log('vm.token' + vm.token);
-    console.log('vm.catid' + vm.catid);
+
     vm.form = 'login';
     vm.submit = function() {
-        // OrderCloud.BuyerID.Set(buyerid);
+         OrderCloud.BuyerID.Set(buyerid);
         OrderCloudSDK.Auth.SetToken(vm.token);
-        /*if(vm.catid){
-        	console.log('Inside if Submit');
-          $state.go('categoryBrowse', {'categoryid':vm.catid});
-        }*/
-        //else {
-        	console.log('Inside else Submit');
-        //  $state.go('categoryBrowse',{'categoryID': 'MainCatalog'});
-        	$state.go('productBrowse.products', {});
-        //	$state.go('productBrowse.products', {'categoryid':vm.catid});
-        //}
     };
 
     var loginTest = function(response) {
       var loginCheck = response.data;
-      console.log('loginCheck' + loginCheck);
+      
       if(loginCheck){
         vm.submit();
       }
     }
-    console.log('Inside Checklogin block');
-    console.log('loginTest' + loginTest);
+    
     var OneMinuteAgo = new Date().getTime() - 60000;
-    console.log('Time Stamps' + OneMinuteAgo + '****' + vm.timestamp);
+
     if(vm.token && vm.timestamp > OneMinuteAgo){
       $http.get('/checklogin/' + vm.timestamp + '/' + vm.encryptstamp).then(loginTest);
     }
